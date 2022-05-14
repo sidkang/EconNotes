20a3c50df82e8ca1c729cf8ef33e91ea322a6d77
sid <sidskang@gmail.com>
2022-05-15 00:22:26 +0800

    add macro-25 ch07-09 notes halfway

--- note/macro-25/test.py
+++ note/macro-25/test.py
@@ -1,4 +1,25 @@
 # %%
-import pandas as pd
+from plantuml import PlantUML
+from IPython.display import Image
 
+uml = """@startuml
+skinparam defaultTextAlignment center
+participant Consumer as con
+participant Corp as corp
+group Period 1
+ [-> con: <latex>L, k_0</latex>
+ con -> corp: <math>L,k_0</math>
+ corp -> con: <math>w_1L,(1+r_1)k_0</math>
+ [<- con: <math>c_1</math>
+end
+group Period 2
+[-> con: <math>L, k_1</math>
+con -> corp: <math>L,k_1</math>
+corp -> con: <math>w_2L,(1+r_2)k_1</math>
+[<- con: <math>c_2</math>
+end
+@enduml"""
+
+puml = PlantUML('http://www.plantuml.com/plantuml/png/')
+Image(url=puml.get_url(uml))
 # %%
