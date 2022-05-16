71f2bb07f281b8dc0aedba8b995e5602ed734d16
sid <sidskang@gmail.com>
2022-05-16 15:40:51 +0800

    almost finish ch07-08

--- note/macro-25/test.py
+++ note/macro-25/test.py
@@ -23,3 +23,52 @@ end
 puml = PlantUML('http://www.plantuml.com/plantuml/png/')
 Image(url=puml.get_url(uml))
 # %%
+from scipy import optimize
+
+def func(x):
+    return [x[0] + x[2] - 2,
+            x[1] - x[2] - x[2]**0.5,
+            (x[1]/x[0])**2 - 0.98 * (0.5 * x[2]**0.5 + 1)]
+solve = optimize.root(func, [0.1, 0.1, 0.1], method='df-sane')
+print(solve.x)
+print(0.5 * solve.x[2] ** -0.5)
+# %%
+
+from scipy import optimize
+
+def func(x):
+    return [x[0] + x[2] - 2,
+            x[1] - x[2] - x[2]**0.4,
+            (x[1]/x[0])**2 - 0.98 * (0.4 * x[2]**0.6 + 1)]
+solve = optimize.root(func, [0.1, 0.1, 0.1], method='hybr')
+print(solve.x)
+print(0.4 * solve.x[2] ** -0.6)
+
+# %%
+
+from scipy import optimize
+
+def func(x):
+    return [(((x[0]**0.4 + x[0])/(2-x[0]))**2 - 0.98 * (0.4 * x[0]**0.6 + 1))**2]
+solve = optimize.root(func, [0.1], method='hybr')
+print(solve.x)
+# print(0.4 * solve.x[2] ** -0.6)
+# %%
+
+from scipy.optimize import root
+
+def func(x):
+    return (( (x**0.5 + x)/(x-2) )**2 - 0.98 * (0.5 * (x**-0.5) + 1))**2
+
+sol = root(func, 1)
+k = sol.x
+c1 = 2 - k
+c2 = k**0.4 + k
+
+# %%
+
+from sympy.solvers import solve
+from sympy import Symbol
+x = Symbol('x')
+solve( ((x**0.5+x)/(2-x))**2 - 0.98 * (x**-0.5*0.5 + 1), x)
+# %%
